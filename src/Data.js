27f79a12629216f5b29837901edec3581ae2a49a
Iliya Ikasi Ephraim Salama <82559303+IliyaIkasi@users.noreply.github.com>
2022-05-24 22:24:19 +0100

    removed data.js

--- src/Data.js
+++ src/Data.js
@@ -1,272 +1,272 @@
-import CAM from "./Images/Crops&Machines.jpg";
-import CAM_ALT from "./Images/CAM.png";
-import FB from "./Images/Cloned-Facebook.png";
-import SMS from "./Images/Courses.png";
-export const projects = [
-	{
-		id: 1,
-		name: "Crops and Machines",
-		image: CAM,
-		imageAlt: CAM_ALT,
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-		usedTech: [
-			{
-				tech1: "JavaScript",
-				tech2: "Java",
-				tech3: "Sql",
-				tech4: "TypeScript",
-				tech5: "Python",
-			},
-		],
-		github: "https://github.com/IliyaIkasi",
-		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
-	},
-	{
-		id: 2,
-		name: "Cloned FaceBook",
-		image: FB,
-		imageAlt: CAM_ALT,
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-		usedTech: [
-			{
-				tech1: "JavaScript",
-				tech2: "Java",
-				tech3: "Sql",
-				tech4: "TypeScript",
-				tech5: "Python",
-			},
-		],
-		github: "https://github.com/IliyaIkasi",
-		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
-	},
-	{
-		id: 3,
-		name: "Student Management System",
-		image: SMS,
-		imageAlt: CAM_ALT,
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-		usedTech: [
-			{
-				tech1: "JavaScript",
-				tech2: "Java",
-				tech3: "Sql",
-				tech4: "TypeScript",
-				tech5: "Python",
-			},
-		],
-		github: "https://github.com/IliyaIkasi",
-		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
-	},
-	{
-		id: 4,
-		name: "Cloned FaceBook",
-		image: FB,
-		imageAlt: CAM_ALT,
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-		usedTech: [
-			{
-				tech1: "JavaScript",
-				tech2: "Java",
-				tech3: "Sql",
-				tech4: "TypeScript",
-				tech5: "Python",
-			},
-		],
-		github: "https://github.com/IliyaIkasi",
-		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
-	},
-	{
-		id: 5,
-		name: "Agba Akin Website Challenge",
-		image: SMS,
-		imageAlt: CAM_ALT,
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-		usedTech: [
-			{
-				tech1: "JavaScript",
-				tech2: "Java",
-				tech3: "Sql",
-				tech4: "TypeScript",
-				tech5: "Python",
-			},
-		],
-		github: "https://github.com/IliyaIkasi",
-		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
-	},
-	{
-		id: 6,
-		name: "Crops and Machines",
-		image: CAM,
-		imageAlt: CAM_ALT,
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-		usedTech: [
-			{
-				tech1: "JavaScript",
-				tech2: "Java",
-				tech3: "Sql",
-				tech4: "TypeScript",
-				tech5: "Python",
-			},
-		],
-		github: "https://github.com/IliyaIkasi",
-		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
-	},
-	{
-		id: 7,
-		name: "Cloned FaceBook",
-		image: FB,
-		imageAlt: CAM_ALT,
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-		usedTech: [
-			{
-				tech1: "JavaScript",
-				tech2: "Java",
-				tech3: "Sql",
-				tech4: "TypeScript",
-				tech5: "Python",
-			},
-		],
-		github: "https://github.com/IliyaIkasi",
-		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
-	},
-	{
-		id: 8,
-		name: "Student Management System",
-		image: SMS,
-		imageAlt: CAM_ALT,
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-		usedTech: [
-			{
-				tech1: "JavaScript",
-				tech2: "Java",
-				tech3: "Sql",
-				tech4: "TypeScript",
-				tech5: "Python",
-			},
-		],
-		github: "https://github.com/IliyaIkasi",
-		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
-	},
-	{
-		id: 9,
-		name: "Crops and Machines",
-		image: CAM,
-		imageAlt: CAM_ALT,
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-		usedTech: [
-			{
-				tech1: "JavaScript",
-				tech2: "Java",
-				tech3: "Sql",
-				tech4: "TypeScript",
-				tech5: "Python",
-			},
-		],
-		github: "https://github.com/IliyaIkasi",
-		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
-	},
-];
+// import CAM from "./Images/Crops&Machines.jpg";
+// import CAM_ALT from "./Images/CAM.png";
+// import FB from "./Images/Cloned-Facebook.png";
+// import SMS from "./Images/Courses.png";
+// export const projects = [
+// 	{
+// 		id: 1,
+// 		name: "Crops and Machines",
+// 		image: CAM,
+// 		imageAlt: CAM_ALT,
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 		usedTech: [
+// 			{
+// 				tech1: "JavaScript",
+// 				tech2: "Java",
+// 				tech3: "Sql",
+// 				tech4: "TypeScript",
+// 				tech5: "Python",
+// 			},
+// 		],
+// 		github: "https://github.com/IliyaIkasi",
+// 		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
+// 	},
+// 	{
+// 		id: 2,
+// 		name: "Cloned FaceBook",
+// 		image: FB,
+// 		imageAlt: CAM_ALT,
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 		usedTech: [
+// 			{
+// 				tech1: "JavaScript",
+// 				tech2: "Java",
+// 				tech3: "Sql",
+// 				tech4: "TypeScript",
+// 				tech5: "Python",
+// 			},
+// 		],
+// 		github: "https://github.com/IliyaIkasi",
+// 		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
+// 	},
+// 	{
+// 		id: 3,
+// 		name: "Student Management System",
+// 		image: SMS,
+// 		imageAlt: CAM_ALT,
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 		usedTech: [
+// 			{
+// 				tech1: "JavaScript",
+// 				tech2: "Java",
+// 				tech3: "Sql",
+// 				tech4: "TypeScript",
+// 				tech5: "Python",
+// 			},
+// 		],
+// 		github: "https://github.com/IliyaIkasi",
+// 		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
+// 	},
+// 	{
+// 		id: 4,
+// 		name: "Cloned FaceBook",
+// 		image: FB,
+// 		imageAlt: CAM_ALT,
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 		usedTech: [
+// 			{
+// 				tech1: "JavaScript",
+// 				tech2: "Java",
+// 				tech3: "Sql",
+// 				tech4: "TypeScript",
+// 				tech5: "Python",
+// 			},
+// 		],
+// 		github: "https://github.com/IliyaIkasi",
+// 		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
+// 	},
+// 	{
+// 		id: 5,
+// 		name: "Agba Akin Website Challenge",
+// 		image: SMS,
+// 		imageAlt: CAM_ALT,
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 		usedTech: [
+// 			{
+// 				tech1: "JavaScript",
+// 				tech2: "Java",
+// 				tech3: "Sql",
+// 				tech4: "TypeScript",
+// 				tech5: "Python",
+// 			},
+// 		],
+// 		github: "https://github.com/IliyaIkasi",
+// 		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
+// 	},
+// 	{
+// 		id: 6,
+// 		name: "Crops and Machines",
+// 		image: CAM,
+// 		imageAlt: CAM_ALT,
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 		usedTech: [
+// 			{
+// 				tech1: "JavaScript",
+// 				tech2: "Java",
+// 				tech3: "Sql",
+// 				tech4: "TypeScript",
+// 				tech5: "Python",
+// 			},
+// 		],
+// 		github: "https://github.com/IliyaIkasi",
+// 		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
+// 	},
+// 	{
+// 		id: 7,
+// 		name: "Cloned FaceBook",
+// 		image: FB,
+// 		imageAlt: CAM_ALT,
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 		usedTech: [
+// 			{
+// 				tech1: "JavaScript",
+// 				tech2: "Java",
+// 				tech3: "Sql",
+// 				tech4: "TypeScript",
+// 				tech5: "Python",
+// 			},
+// 		],
+// 		github: "https://github.com/IliyaIkasi",
+// 		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
+// 	},
+// 	{
+// 		id: 8,
+// 		name: "Student Management System",
+// 		image: SMS,
+// 		imageAlt: CAM_ALT,
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 		usedTech: [
+// 			{
+// 				tech1: "JavaScript",
+// 				tech2: "Java",
+// 				tech3: "Sql",
+// 				tech4: "TypeScript",
+// 				tech5: "Python",
+// 			},
+// 		],
+// 		github: "https://github.com/IliyaIkasi",
+// 		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
+// 	},
+// 	{
+// 		id: 9,
+// 		name: "Crops and Machines",
+// 		image: CAM,
+// 		imageAlt: CAM_ALT,
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 		usedTech: [
+// 			{
+// 				tech1: "JavaScript",
+// 				tech2: "Java",
+// 				tech3: "Sql",
+// 				tech4: "TypeScript",
+// 				tech5: "Python",
+// 			},
+// 		],
+// 		github: "https://github.com/IliyaIkasi",
+// 		live: "https://www.linkedin.com/in/iliya-ikasi-a30a02217/",
+// 	},
+// ];
 
-export const experience = [
-	{
-		id: 1,
-		org: "Apple",
-		role: "Java Developer",
-		sDate: "January 2020",
-		eDate: "Present",
-		status: "one",
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
-			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
-			"deserunt dolore vel, maiores mollitia id, enim repellat," +
-			"exercitationem dolores cumque alias repellendus?",
-	},
-	{
-		id: 2,
-		org: "Microsoft",
-		role: "Python Developer",
-		sDate: "February 2020",
-		eDate: "Present",
-		status: "two",
-		desc:
-			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. " +
-			"Eius facilis sunt similique saepe, cumque, blanditiis dolorum ea velit " +
-			"aliquam deleniti obcaecati cupiditate autem repellendus voluptates? " +
-			"Nihil cum quia officia ad!",
-	},
-	{
-		id: 3,
-		org: "Dell",
-		role: "JavaScript Developer",
-		sDate: "March 2020",
-		eDate: "Present",
-		status: "three",
-		desc:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. " +
-			"Quasi, perspiciatis dolore est tenetur, laudantium necessitatibus consequuntur" +
-			"provident quidem adipisci nihil, repellendus molestias molestiae delectus. " +
-			"Numquam maxime perspiciatis tempora corporis quasi.",
-	},
-	{
-		id: 4,
-		org: "Samsung",
-		role: "TypeScript Developer",
-		sDate: "April 2020",
-		eDate: "Present",
-		status: "inactive",
-		desc:
-			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. " +
-			"Consequuntur ipsa adipisci, saepe cum dignissimos magnam veritatis eligendi, " +
-			"illum accusantium dicta vitae dolore tempora velit quisquam aspernatur! " +
-			"Veritatis eius soluta ab!",
-	},
-	{
-		id: 5,
-		org: "HP",
-		role: "SQL Developer",
-		sDate: "May 2020",
-		eDate: "Present",
-		status: "active",
-		desc:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
-			"Odio facere dolor delectus iste sequi cumque facilis earum beatae, " +
-			"voluptas quaerat dignissimos tenetur natus deleniti incidunt nihil " +
-			"explicabo illum a praesentium!",
-	},
-];
+// export const experience = [
+// 	{
+// 		id: 1,
+// 		org: "Apple",
+// 		role: "Java Developer",
+// 		sDate: "January 2020",
+// 		eDate: "Present",
+// 		status: "one",
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. A et nemo" +
+// 			"similique deleniti, accusantium necessitatibus odit. Accusamus" +
+// 			"deserunt dolore vel, maiores mollitia id, enim repellat," +
+// 			"exercitationem dolores cumque alias repellendus?",
+// 	},
+// 	{
+// 		id: 2,
+// 		org: "Microsoft",
+// 		role: "Python Developer",
+// 		sDate: "February 2020",
+// 		eDate: "Present",
+// 		status: "two",
+// 		desc:
+// 			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. " +
+// 			"Eius facilis sunt similique saepe, cumque, blanditiis dolorum ea velit " +
+// 			"aliquam deleniti obcaecati cupiditate autem repellendus voluptates? " +
+// 			"Nihil cum quia officia ad!",
+// 	},
+// 	{
+// 		id: 3,
+// 		org: "Dell",
+// 		role: "JavaScript Developer",
+// 		sDate: "March 2020",
+// 		eDate: "Present",
+// 		status: "three",
+// 		desc:
+// 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. " +
+// 			"Quasi, perspiciatis dolore est tenetur, laudantium necessitatibus consequuntur" +
+// 			"provident quidem adipisci nihil, repellendus molestias molestiae delectus. " +
+// 			"Numquam maxime perspiciatis tempora corporis quasi.",
+// 	},
+// 	{
+// 		id: 4,
+// 		org: "Samsung",
+// 		role: "TypeScript Developer",
+// 		sDate: "April 2020",
+// 		eDate: "Present",
+// 		status: "inactive",
+// 		desc:
+// 			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. " +
+// 			"Consequuntur ipsa adipisci, saepe cum dignissimos magnam veritatis eligendi, " +
+// 			"illum accusantium dicta vitae dolore tempora velit quisquam aspernatur! " +
+// 			"Veritatis eius soluta ab!",
+// 	},
+// 	{
+// 		id: 5,
+// 		org: "HP",
+// 		role: "SQL Developer",
+// 		sDate: "May 2020",
+// 		eDate: "Present",
+// 		status: "active",
+// 		desc:
+// 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+// 			"Odio facere dolor delectus iste sequi cumque facilis earum beatae, " +
+// 			"voluptas quaerat dignissimos tenetur natus deleniti incidunt nihil " +
+// 			"explicabo illum a praesentium!",
+// 	},
+// ];
